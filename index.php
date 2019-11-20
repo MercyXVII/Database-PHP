088d0504dde86ffb2367afc8bef39b3912707400
Martijn <127121@student.drenthecollege.nl>
2019-11-20 11:00:21 +0100

    Database connection made

--- index.php
+++ index.php
@@ -9,9 +9,14 @@
       $servername = "localhost";
       $databasename = "db_level2_opdr1";
       $username = "Martijn";
-      $password = "welkom1234";
+      $password = "Welkom1234";
 
+      // Create connection
+      $conn = new mysqli($servername, $username, $password, $databasename);
 
+      if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error); }
+          echo "Connected successfully";
 
 
 
