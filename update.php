1a579373a2fd532e66a03a3bf1dd641bf9b61e15
Martijn <127121@student.drenthecollege.nl>
2019-11-27 09:12:01 +0100

    css done

--- update.php
+++ update.php
@@ -2,9 +2,10 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>Insert</title>
+    <link rel="stylesheet" href="./CSS/master.css">
   </head>
-  <body>
+  <body id="addnew">
 
     <?php
       error_reporting(0);
@@ -16,20 +17,21 @@
       $result = $conn->query($sql);
       $row = $result->fetch_assoc();
 
-      echo "Het geselecteerde nummer is: ".$row['artist']." - ".$row['title'];
+      echo "<h1>Het geselecteerde nummer is: ".$row['artist']." - ".$row['title']."</h1>";
     ?>
 
     <form method="post">
       <label for="artist">Artist:</label>
       <input type="text" name="artist">
       <label for="title">Title:</label>
-      <input type="text" name="title">
+      <input type="text" name="title"> <br>
       <input type="submit" name="submit">
     </form>
 
     <div id="form">
       <p><a href="index.php">Ga terug naar het overzicht</a></p>
       <p><a href="addnew.php">Insert New Song</a></p>
+    </div>
 
     <?php
     $artist = $_POST['artist'];
